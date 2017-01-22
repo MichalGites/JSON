32f6ad34bd8ec84f6895a28308996fe87ebf81f6
Michal Berezanski <michal.berezanski@op.pl>
2017-01-22 23:47:22 +0100

    GoogleMaps JSON file reading task

--- app/src/main/java/pl/akademiakodu/json/JSONParser.java
+++ app/src/main/java/pl/akademiakodu/json/JSONParser.java
@@ -16,29 +16,16 @@ public class JSONParser {
     public JSONParser(){}
 
     public void readJSON (String s){
-        // walidujemy dane po kolei najpierw obiekt JSON
-            try {
-                JSONObject bigO = new JSONObject(s);
-                // następnie główna tablica - argument to nazwa tej tablicy
-                JSONArray dataArray = bigO.getJSONArray("data");
-                // pętla po wszystkich obiektach (wierszach) tablicy
-                for (int i=0; i<dataArray.length();i++){
-                    // dostęp do pojedynczego wiersza głównej tablicy
-                    JSONObject row = dataArray.getJSONObject(i);
-                    // dostęp do tablicy będącej elementem pojedynczego wiersza tabluicy głównej
-                    JSONArray actionsArray = row.getJSONArray("actions");
-                    // pętla po wszytskich wierszach tej tablicy podrzędnej
-                    for (int x = 0; x<actionsArray.length();x++){
-                        // dostęp do pojedynczego wiersza tablicy podrzędnej
-                        JSONObject rowActions = actionsArray.getJSONObject(x);
-                        // wyświetlenie danych z pojedynczego wiersza tablicy podrzędnej
-                        Log.e("debug", "Nazwa akcji: "+rowActions.getString("name"));
-                        Log.e("debug", "Link akcji: "+rowActions.getString("link"));
-                    }
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
+        try {
+            JSONObject bigO = new JSONObject(s);
+            JSONArray markersArray = bigO.getJSONArray("markers");
+            for (int i = 0; i<markersArray.length();i++) {
+                JSONObject row = markersArray.getJSONObject(i);
+                Log.e("debug", "Informacja: "+row.getString("information"));
             }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
 }
--- app/src/main/java/pl/akademiakodu/json/MainActivity.java
+++ app/src/main/java/pl/akademiakodu/json/MainActivity.java
@@ -14,49 +14,37 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         JSONParser parser = new JSONParser();
-        parser.readJSON("{\n" +
-                "   \"data\": [\n" +
-                "      {\n" +
-                "         \"id\": \"X999_Y999\",\n" +
-                "         \"from\": {\n" +
-                "            \"name\": \"Tom Brady\", \"id\": \"X12\"\n" +
-                "         },\n" +
-                "         \"message\": \"Looking forward to 2010!\",\n" +
-                "         \"actions\": [\n" +
-                "            {\n" +
-                "               \"name\": \"Comment\",\n" +
-                "               \"link\": \"http://www.facebook.com/X999/posts/Y999\"\n" +
-                "            },\n" +
-                "            {\n" +
-                "               \"name\": \"Like\",\n" +
-                "               \"link\": \"http://www.facebook.com/X999/posts/Y999\"\n" +
-                "            }\n" +
-                "         ],\n" +
-                "         \"type\": \"status\",\n" +
-                "         \"created_time\": \"2010-08-02T21:27:44+0000\",\n" +
-                "         \"updated_time\": \"2010-08-02T21:27:44+0000\"\n" +
-                "      },\n" +
-                "      {\n" +
-                "         \"id\": \"X998_Y998\",\n" +
-                "         \"from\": {\n" +
-                "            \"name\": \"Peyton Manning\", \"id\": \"X18\"\n" +
-                "         },\n" +
-                "         \"message\": \"Where's my contract?\",\n" +
-                "         \"actions\": [\n" +
-                "            {\n" +
-                "               \"name\": \"Comment\",\n" +
-                "               \"link\": \"http://www.facebook.com/X998/posts/Y998\"\n" +
-                "            },\n" +
-                "            {\n" +
-                "               \"name\": \"Like\",\n" +
-                "               \"link\": \"http://www.facebook.com/X998/posts/Y998\"\n" +
-                "            }\n" +
-                "         ],\n" +
-                "         \"type\": \"status\",\n" +
-                "         \"created_time\": \"2010-08-02T21:27:44+0000\",\n" +
-                "         \"updated_time\": \"2010-08-02T21:27:44+0000\"\n" +
-                "      }\n" +
-                "   ]\n" +
-                "}");
+        parser.readJSON("{\"markers\": [\n" +
+                "\t\t{\n" +
+                "\t\t\t\"point\":\"new GLatLng(40.266044,-74.718479)\", \n" +
+                "\t\t\t\"homeTeam\":\"Lawrence Library\",\n" +
+                "\t\t\t\"awayTeam\":\"LUGip\",\n" +
+                "\t\t\t\"markerImage\":\"images/red.png\",\n" +
+                "\t\t\t\"information\": \"Linux users group meets second Wednesday of each month.\",\n" +
+                "\t\t\t\"fixture\":\"Wednesday 7pm\",\n" +
+                "\t\t\t\"capacity\":\"\",\n" +
+                "\t\t\t\"previousScore\":\"\"\n" +
+                "\t\t},\n" +
+                "\t\t{\n" +
+                "\t\t\t\"point\":\"new GLatLng(40.211600,-74.695702)\",\n" +
+                "\t\t\t\"homeTeam\":\"Hamilton Library\",\n" +
+                "\t\t\t\"awayTeam\":\"LUGip HW SIG\",\n" +
+                "\t\t\t\"markerImage\":\"images/white.png\",\n" +
+                "\t\t\t\"information\": \"Linux users can meet the first Tuesday of the month to work out harward and configuration issues.\",\n" +
+                "\t\t\t\"fixture\":\"Tuesday 7pm\",\n" +
+                "\t\t\t\"capacity\":\"\",\n" +
+                "\t\t\t\"tv\":\"\"\n" +
+                "\t\t},\n" +
+                "\t\t{\n" +
+                "\t\t\t\"point\":\"new GLatLng(40.294535,-74.682012)\",\n" +
+                "\t\t\t\"homeTeam\":\"Applebees\",\n" +
+                "\t\t\t\"awayTeam\":\"After LUPip Mtg Spot\",\n" +
+                "\t\t\t\"markerImage\":\"images/newcastle.png\",\n" +
+                "\t\t\t\"information\": \"Some of us go there after the main LUGip meeting, drink brews, and talk.\",\n" +
+                "\t\t\t\"fixture\":\"Wednesday whenever\",\n" +
+                "\t\t\t\"capacity\":\"2 to 4 pints\",\n" +
+                "\t\t\t\"tv\":\"\"\n" +
+                "\t\t}\n" +
+                "] }");
     }
 }
