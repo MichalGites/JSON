7ee406990e4df92157b19a669a92653312a86781
Michal Berezanski <michal.berezanski@op.pl>
2017-02-19 23:34:01 +0100

    JSON file read via URL connection

--- app/src/main/java/pl/akademiakodu/json/JSONParser.java
+++ app/src/main/java/pl/akademiakodu/json/JSONParser.java
@@ -1,10 +1,20 @@
 package pl.akademiakodu.json;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by Michał on 2017-01-22.
@@ -13,7 +23,11 @@ import org.json.JSONObject;
 public class JSONParser {
 
     // pusty konstruktor
-    public JSONParser(){}
+    public JSONParser(){
+
+        new AsyncWWW().execute("http://projekt.techloft.pl/akademiakodu/dane.php");
+
+    }
 
     public void readJSON (String s){
         try {
@@ -25,6 +39,54 @@ public class JSONParser {
             }
         } catch (JSONException e) {
             e.printStackTrace();
+        }
+    }
+
+    // operacje na Internecie w Androidzie robimy zawsze w oddzielnym watku a wiec AsyncTask
+
+    public String makeCall (String url){
+        String response = null;
+        // konwersja ciagu znakow na URL
+        StringBuilder builder = new StringBuilder();
+        Log.e("internet", "Lacze ze strona internetowa");
+        try {
+            URL trueUrl = new URL (url);
+            // strumieniowe otwarcie polaczenia ze strona internetowa
+            HttpURLConnection conn = (HttpURLConnection) trueUrl.openConnection();
+            // przechwycenie strumienia polaczenia i odczytanie danych z niego
+            InputStream inputStream = new BufferedInputStream(conn.getInputStream());
+            // BufferedReader czyli wszystkie linijki naszej strony
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+            Log.e("internet", "Pobieram dane ze strony");
+            while ((response = bufferedReader.readLine()) != null){
+                // za pomoca buildera odczytujemy dane ze strony dodajac przypisane response typu String oraz nowa linie
+                // zeby kollejna linijka strony byla kolejna linijka buildera, nie zleje sie w jedno dzieki temu
+                builder.append(response).append("\n");
+            }
+            inputStream.close();
+            Log.e("internet", builder.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return builder.toString();
+
+    }
+
+    private class AsyncWWW extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.e("internet", "Uruchomienie z poziomu innego watku");
+            return makeCall(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+            readJSON(s);
         }
     }
 
--- app/src/main/java/pl/akademiakodu/json/MainActivity.java
+++ app/src/main/java/pl/akademiakodu/json/MainActivity.java
@@ -16,37 +16,37 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         JSONParser parser = new JSONParser();
-        parser.readJSON("{\"markers\": [\n" +
-                "\t\t{\n" +
-                "\t\t\t\"point\":\"new GLatLng(40.266044,-74.718479)\", \n" +
-                "\t\t\t\"homeTeam\":\"Lawrence Library\",\n" +
-                "\t\t\t\"awayTeam\":\"LUGip\",\n" +
-                "\t\t\t\"markerImage\":\"images/red.png\",\n" +
-                "\t\t\t\"information\": \"Linux users group meets second Wednesday of each month.\",\n" +
-                "\t\t\t\"fixture\":\"Wednesday 7pm\",\n" +
-                "\t\t\t\"capacity\":\"\",\n" +
-                "\t\t\t\"previousScore\":\"\"\n" +
-                "\t\t},\n" +
-                "\t\t{\n" +
-                "\t\t\t\"point\":\"new GLatLng(40.211600,-74.695702)\",\n" +
-                "\t\t\t\"homeTeam\":\"Hamilton Library\",\n" +
-                "\t\t\t\"awayTeam\":\"LUGip HW SIG\",\n" +
-                "\t\t\t\"markerImage\":\"images/white.png\",\n" +
-                "\t\t\t\"information\": \"Linux users can meet the first Tuesday of the month to work out harward and configuration issues.\",\n" +
-                "\t\t\t\"fixture\":\"Tuesday 7pm\",\n" +
-                "\t\t\t\"capacity\":\"\",\n" +
-                "\t\t\t\"tv\":\"\"\n" +
-                "\t\t},\n" +
-                "\t\t{\n" +
-                "\t\t\t\"point\":\"new GLatLng(40.294535,-74.682012)\",\n" +
-                "\t\t\t\"homeTeam\":\"Applebees\",\n" +
-                "\t\t\t\"awayTeam\":\"After LUPip Mtg Spot\",\n" +
-                "\t\t\t\"markerImage\":\"images/newcastle.png\",\n" +
-                "\t\t\t\"information\": \"Some of us go there after the main LUGip meeting, drink brews, and talk.\",\n" +
-                "\t\t\t\"fixture\":\"Wednesday whenever\",\n" +
-                "\t\t\t\"capacity\":\"2 to 4 pints\",\n" +
-                "\t\t\t\"tv\":\"\"\n" +
-                "\t\t}\n" +
-                "] }");
+//        parser.readJSON("{\"markers\": [\n" +
+//                "\t\t{\n" +
+//                "\t\t\t\"point\":\"new GLatLng(40.266044,-74.718479)\", \n" +
+//                "\t\t\t\"homeTeam\":\"Lawrence Library\",\n" +
+//                "\t\t\t\"awayTeam\":\"LUGip\",\n" +
+//                "\t\t\t\"markerImage\":\"images/red.png\",\n" +
+//                "\t\t\t\"information\": \"Linux users group meets second Wednesday of each month.\",\n" +
+//                "\t\t\t\"fixture\":\"Wednesday 7pm\",\n" +
+//                "\t\t\t\"capacity\":\"\",\n" +
+//                "\t\t\t\"previousScore\":\"\"\n" +
+//                "\t\t},\n" +
+//                "\t\t{\n" +
+//                "\t\t\t\"point\":\"new GLatLng(40.211600,-74.695702)\",\n" +
+//                "\t\t\t\"homeTeam\":\"Hamilton Library\",\n" +
+//                "\t\t\t\"awayTeam\":\"LUGip HW SIG\",\n" +
+//                "\t\t\t\"markerImage\":\"images/white.png\",\n" +
+//                "\t\t\t\"information\": \"Linux users can meet the first Tuesday of the month to work out harward and configuration issues.\",\n" +
+//                "\t\t\t\"fixture\":\"Tuesday 7pm\",\n" +
+//                "\t\t\t\"capacity\":\"\",\n" +
+//                "\t\t\t\"tv\":\"\"\n" +
+//                "\t\t},\n" +
+//                "\t\t{\n" +
+//                "\t\t\t\"point\":\"new GLatLng(40.294535,-74.682012)\",\n" +
+//                "\t\t\t\"homeTeam\":\"Applebees\",\n" +
+//                "\t\t\t\"awayTeam\":\"After LUPip Mtg Spot\",\n" +
+//                "\t\t\t\"markerImage\":\"images/newcastle.png\",\n" +
+//                "\t\t\t\"information\": \"Some of us go there after the main LUGip meeting, drink brews, and talk.\",\n" +
+//                "\t\t\t\"fixture\":\"Wednesday whenever\",\n" +
+//                "\t\t\t\"capacity\":\"2 to 4 pints\",\n" +
+//                "\t\t\t\"tv\":\"\"\n" +
+//                "\t\t}\n" +
+//                "] }");
     }
 }
